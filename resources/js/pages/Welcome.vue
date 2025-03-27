2916c5dc782868964d35217c566b7637053030c5
Charlie Hield <charles.hield@gmail.com>
2025-03-27 15:57:58 -0500

    Wire up welcome links

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { StepperItem } from '@nuxt/ui'
+import { Head, Link } from '@inertiajs/vue3'
 
 const active = ref(-1)
 
@@ -24,6 +25,7 @@ const items = ref<StepperItem[]>([
 </script>
 
 <template>
+  <Head title="Welcome" />
   <div class="flex items-center justify-center p-12 lg:h-screen">
     <UCard
       class="flex w-full max-w-sm flex-col overflow-hidden lg:max-w-4xl lg:flex-row-reverse"
@@ -44,8 +46,12 @@ const items = ref<StepperItem[]>([
           class="w-full"
         />
         <div class="flex gap-4">
-          <UButton variant="soft" block>Log in</UButton>
-          <UButton block>Register</UButton>
+          <Link :href="route('login')" class="w-full">
+            <UButton variant="soft" block>Log in</UButton>
+          </Link>
+          <Link :href="route('register')" class="w-full">
+            <UButton block>Register</UButton>
+          </Link>
         </div>
       </div>
     </UCard>
